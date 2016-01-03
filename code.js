c1c3a39d323c19c7fab4ebcf873a8f686fbd21c5
David Jones <drj@pobox.com>
2016-01-03 17:24:19 +0000

    Remove unused vars

--- code.js
+++ code.js
@@ -8,14 +8,6 @@ var gl;
 var cubeVerticesBuffer;
 var cubeVerticesColorBuffer;
 var cubeVerticesIndexBuffer;
-var cubeRotation = 0.0;
-var cubeXOffset = 0.0;
-var cubeYOffset = 0.0;
-var cubeZOffset = 0.0;
-var lastCubeUpdateTime = 0;
-var xIncValue = 0.2;
-var yIncValue = -0.4;
-var zIncValue = 0.3;
 
 var mvMatrix;
 var shaderProgram;
