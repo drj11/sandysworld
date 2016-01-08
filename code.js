a9893625d2a9b5eb35bfd4f3d5bb4aca64cc6d78
David Jones <drj@pobox.com>
2016-01-08 21:00:20 +0000

    Change the clear colour to yellow

--- code.js
+++ code.js
@@ -133,7 +133,7 @@ function start() {
   // Only continue if WebGL is available and working
 
   if (gl) {
-    gl.clearColor(0.0, 1.0, 1.0, 1.0);  // Clear to cyan, fully opaque
+    gl.clearColor(1.0, 1.0, 0.4, 1.0);  // Clear to "yellow"
     gl.clearDepth(1.0);                 // Clear everything
     gl.enable(gl.DEPTH_TEST);           // Enable depth testing
     gl.depthFunc(gl.LEQUAL);            // Near things obscure far
